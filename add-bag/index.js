98984069390c4e9b1a19fc86c7b1242e00ba2635
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2022-12-28 15:49:58 +0530

    myntra_Clone

---
+++ add-bag/index.js
@@ -0,0 +1,84 @@
+const logo = document.querySelector(".logo");
+logo.addEventListener("click", () => {
+  location = "../index.html";
+});
+
+// ================================================
+
+const mainBag = document.querySelector(".product-bag");
+const totalAmount = document.querySelector("#total-amount");
+const totalAmountDiscount = document.querySelector("#total-amount-discount");
+const totalItem = document.querySelector("#total-item");
+window.addEventListener("load", () => {
+  let a = localStorage.getItem("productArray");
+  let productObj = JSON.parse(a);
+  productObj.forEach((product) => {
+    mainBag.innerHTML += ` <div class="product">
+        <div class="image"><img src="${product.itemLink}" alt=""></div>
+        <div class="description">
+            <div class="item-name">${product.itemName}</div>
+            <div class="item-description">${product.description}</div>
+            <div class="size">Size:
+                <select name="size" id="quantity">
+                    <option value="M">S</option>
+                    <option value="M">M</option>
+                    <option value="M">L</option>
+                    <option value="M">XL</option>
+                </select>
+        </div>
+            <div class="quantity">Qty:
+                <select name="quantity" id="quantity">
+                    <option value="M">1</option>
+                    <option value="M">2</option>
+                    <option value="M">3</option>
+                    <option value="M">4</option>
+                </select>
+            </div>
+           <div class="remove-box"><div class="price"><span>Rs.</span><span>${product.price}</span></div><button class="remove-btn">Remove</button></div>
+        </div>
+    </div>`;
+  });
+  let price = 0;
+  productObj.forEach((product) => {
+    let p = Number(product.price);
+    price += p;
+    console.log(p);
+  });
+  let total = productObj.length;
+  totalAmount.innerText = price; //totalAmount
+  totalAmountDiscount.innerText = price; //totalAmountDiscount
+  totalItem.innerText = total; //totalItem
+  localStorage.setItem("totalMrp", price);
+  localStorage.setItem("totalProduct", total);
+});
+
+// function to remove item
+
+mainBag.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-btn")) {
+    e.target.parentNode.parentNode.parentNode.remove();
+    let total = localStorage.getItem("totalProduct"); //setting new total product after delete
+    total--;
+    totalItem.innerText = total;
+    localStorage.setItem("totalProduct", total); //set totalItem in localstorage after removing item
+    let src =
+      e.target.parentNode.parentNode.parentNode.children[0].children[0].src;
+    let a = localStorage.getItem("productArray") || "[]";
+    let productObj = JSON.parse(a);
+    productObj.forEach((storeProduct, i) => {
+      if (src == storeProduct.itemLink) {
+        console.log(storeProduct);
+        productObj.splice(i, 1);
+      }
+    });
+    localStorage.setItem("productArray", JSON.stringify(productObj));
+    let price = 0;
+    productObj.forEach((product) => {
+      let p = Number(product.price);
+      price += p;
+      console.log(p);
+    });
+    totalAmount.innerText = price;
+    totalAmountDiscount.innerText = price; //totalAmountDiscount
+  }
+});
---
+++ otpform/otp.js
@@ -0,0 +1,71 @@
+const number = document.getElementById("numbertosend");
+window.onload = () => {
+  let num = localStorage.getItem("number");
+  number.innerText = `Sent to ${num}`;
+};
+
+const timer = document.getElementById("timer");
+console.log(timer.innerText);
+let time = 30;
+function timerr() {
+  if (time < 1) {
+    timer.innerText = "Resend OTP";
+    clearInterval(i);
+  }
+  timer.innerText = `Resend OTP in 00:${time--}`;
+}
+let i = setInterval(timerr, 1000);
+
+// OTP Generating
+
+let num1 = Math.floor(Math.random() * 10);
+let num2 = Math.floor(Math.random() * 10);
+let num3 = Math.floor(Math.random() * 10);
+let num4 = Math.floor(Math.random() * 10);
+
+const sentOTP = document.querySelector(".otp");
+sentOTP.innerText = `${num1} ${num2} ${num3} ${num4} `;
+console.log(sentOTP.innerText);
+
+// validation for OTP
+
+const otp1 = document.getElementById("otp1");
+const otp2 = document.getElementById("otp2");
+const otp3 = document.getElementById("otp3");
+const otp4 = document.getElementById("otp4");
+
+const wrong = document.getElementById("wrong");
+const btn = document.getElementById("btn");
+
+// function to verify otp and also check that user is already have account or not
+
+btn.addEventListener("click", async () => {
+  let num = localStorage.getItem("number");
+  if (
+    num1 != otp1.value ||
+    num2 != otp2.value ||
+    num3 != otp3.value ||
+    num4 != otp4.value
+  ) {
+    wrong.innerText = "Wrong OTP";
+    setTimeout(() => {
+      wrong.innerText = "";
+    }, 1000);
+  } else {
+    const data = {
+      mobileNumber: num,
+    };
+    const response = await fetch(
+      `https://api-vqd9.onrender.com/users/login?mobileNumber=${num}`
+    );
+    let res = await response.json();
+    if ((res = "User not exists!")) {
+      wrong.innerText = "User not exists!! Create account";
+      setTimeout(() => {
+        location = "../signup_Page/index.html";
+      }, 1000);
+    } else {
+      location = "../index.html";
+    }
+  }
+});
